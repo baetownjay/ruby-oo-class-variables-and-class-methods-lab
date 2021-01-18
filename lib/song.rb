4ffbf1eac3f0a15ea6b7125c02461c60f54c39da
Jason Herrera <baytownjason@yahoo.com>
2021-01-18 14:57:18 -0600

    commit tree 5c4f9b1773a3a6fda32cbd37c64c1a74bdfead88

--- lib/song.rb
+++ lib/song.rb
@@ -3,46 +3,131 @@ class Song
     attr_accessor :name, :artist, :genre
 
     @@count = 0
-    @@artists =[]
+    @@names = []
+    @@artists = []
     @@genres = []
+
+    @@all = []
 
     def initialize(name, artist, genre)
         @name = name
         @artist = artist
         @genre = genre
-        @@artists << artist
-        @@count += 1
-        @@genres << genre
-    end
 
-    def self.count 
+        @@artists << artist
+        @@genres << genre
+        
+        @@count +=1
+        @@names << name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def self.songs
+        @@names
+    end
+    def self.count
         @@count
     end
-
-    def self.genres
-        @@genres.uniq
-    end
-
     def self.artists
         @@artists.uniq
     end
-
+    def self.genres
+        @@genres.uniq
+    end
     def self.genre_count
-        genre_count = Hash.new(0)
-        #create key/value pairs of "genre" = count
-        @@genres.each do |genre| 
-            #binding.pry
-            genre_count[genre] += 1 
+        genre_counter = Hash.new(0)
+        @@genres.map do |genre_category|
+            genre_counter[genre_category] += 1
         end
-        genre_count
+        genre_counter
     end
-
     def self.artist_count
-        artist_count = Hash.new(0)
-        @@artists.each {|artist| artist_count[artist] += 1}
-        artist_count
+        artist_counter = Hash.new(0)
+        @@artists.map do |artist_name|
+            artist_counter[artist_name] += 1
+        end
+        artist_counter
     end
+        
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Song
+#     attr_accessor :name, :artist, :genre
+
+#     @@count = 0
+#     @@artists =[]
+#     @@genres = []
+
+#     def initialize(name, artist, genre)
+#         @name = name
+#         @artist = artist
+#         @genre = genre
+#         @@artists << artist
+#         @@count += 1
+#         @@genres << genre
+#     end
+
+#     def self.count 
+#         @@count
+#     end
+
+#     def self.genres
+#         @@genres.uniq
+#     end
+
+#     def self.artists
+#         @@artists.uniq
+#     end
+
+#     def self.genre_count
+#         genre_count = Hash.new(0)
+#         #create key/value pairs of "genre" = count
+#         @@genres.each do |genre| 
+#             #binding.pry
+#             genre_count[genre] += 1 
+#         end
+#         genre_count
+#     end
+
+#     def self.artist_count
+#         artist_count = Hash.new(0)
+#         @@artists.each {|artist| artist_count[artist] += 1}
+#         artist_count
+#     end
+# end
 
 # require 'pry'
 # class Song
